5c5f1a54d36bbe73de50a87471a0e235bcc1ce0b
Arsela Bita <arselabita@gmail.com>
2025-09-19 21:03:00 +0200

    created server bonus funct

---
+++ src/server_bonus.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server_bonus.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abita <abita@student.42vienna.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/19 20:59:10 by abita             #+#    #+#             */
+/*   Updated: 2025/09/19 20:59:38 by abita            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include "ft_printf.h"
+
+static void handler(int sig)
+{
+    static unsigned char bits = 0;
+    static int bit_position = 0;
+
+    if (sig == SIGUSR1)
+        bits = (bits << 1) | 0;
+    else
+        bits = (bits << 1) | 1;
+    bit_position++;
+    if (bit_position == 8)
+    {
+        if (bits != '\0')
+            write(1, &bits, 1);
+        else
+            write(1, "\n", 1);
+        bits = 0;
+        bit_position = 0;     
+    }
+}
+
+int main()
+{
+    ft_printf("%d\n", (int)getpid());
+    signal(SIGUSR1, handler);
+    signal(SIGUSR2, handler);
+    while (1)
+        pause();
+    return (0);
+}

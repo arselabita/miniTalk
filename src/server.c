c26437d2dbc6f81bed81aa21e4651ea79c1afe72
Arsela Bita <arselabita>
2025-09-22 16:49:30 +0200

    created an in function array to place all the chars then print it

--- src/server.c
+++ src/server.c
@@ -10,13 +10,41 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#define _DEFAULT_SOURCE
 #include <unistd.h>
 #include <signal.h>
 #include <sys/types.h>
 #include "ft_printf.h"
 
-static void handler(int sig)
+#define BUFFER_SIZE 1024
+
+static void printing(unsigned char bits)
 {
+    static char buffer[BUFFER_SIZE];
+    static int i = 0;
+
+    if (bits != '\0')
+    {
+        buffer[i++] = bits;
+        if (i >= BUFFER_SIZE)
+        {
+            write(1, &buffer, i);
+            i = 0;                  
+        }
+    }
+    else
+    {
+        buffer[i++] = '\0';
+        write(1, &buffer, i);
+        write(1, "\n", 1);
+        i = 0;
+    }
+}
+
+static void handler(int sig, siginfo_t *info, void *ucontext)
+{
+    (void)info;
+    (void)ucontext;
     static unsigned char bits = 0;
     static int bit_position = 0;
 
@@ -27,10 +55,7 @@ static void handler(int sig)
     bit_position++;
     if (bit_position == 8)
     {
-        if (bits != '\0')
-            write(1, &bits, 1);
-        else
-            write(1, "\n", 1);
+        printing(bits);
         bits = 0;
         bit_position = 0;     
     }
@@ -38,9 +63,16 @@ static void handler(int sig)
 
 int main()
 {
+    struct sigaction sa;
+
     ft_printf("%d\n", (int)getpid());
-    signal(SIGUSR1, handler);
-    signal(SIGUSR2, handler);
+	sa.sa_sigaction = &handler;
+	sa.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa.sa_mask);
+	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+		return (write(2, "error: sigaction\n", 17), -1);
+	if (sigaction(SIGUSR2, &sa, NULL) == -1)
+		return (write(2, "error: sigaction\n", 17), -1);
     while (1)
         pause();
     return (0);

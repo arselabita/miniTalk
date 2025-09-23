6e6f2a7a8328702739f5a304f5197317585bee6a
Arsela Bita <arselabita>
2025-09-23 19:53:57 +0200

    i will nothandle handshake

--- src/client.c
+++ src/client.c
@@ -15,8 +15,6 @@
 #include <sys/types.h>
 #include "libft.h"
 
-static int signal_received = 0;
-
 static int	ft_valid_number(char *str)
 {
 	int	i;
@@ -36,69 +34,54 @@ static int	ft_valid_number(char *str)
 	}
 	return (1);
 }
-static void encoding(int ascii_value, int pid)
+static int encoding(int ascii_value, int pid)
 {
     int j;
 
     j = 7;
     while (j >= 0)
-    {   
-        if (signal_received == 1)
+    {
+        if ((ascii_value >> j) & 1)
         {
-            if ((ascii_value >> j) & 1)
-            {
-                if (kill(pid, SIGUSR2) == -1)    
-                    exit(EXIT_FAILURE);
-            }
-            else
-            {
-                if (kill(pid, SIGUSR1) == -1)
-                    exit(EXIT_FAILURE);
-            }
-            signal_received = 0;
+            if (kill(pid, SIGUSR2) == -1)    
+                exit(EXIT_FAILURE);
         }
-        usleep(1000);
+        else
+        {
+            if (kill(pid, SIGUSR1) == -1)
+                exit(EXIT_FAILURE);
+        }
         j--;
+        usleep(100);
     }
+    return (0);
 }
-void msg_received(int sig, siginfo_t *info, void *ucontext)
-{
-    (void)ucontext;
-    (void)info;
-    (void)sig;
-    
-    signal_received = 1;
-}
+
 int main(int ac, char **av)
 {
-    struct sigaction sa;
     pid_t pid;
     int i;
 
-    signal_received = 1;
 	if (ac != 3)
-		return (write(2, "ERROR: Write: PID and String!\n", 30), -1);
+		return (write(2, "ERROR: Write: PID and String!\n", 30), 1);
     if (av[1] == NULL)
         return (write(2, "ERROR: Pass the PID!\n", 24), -1);
     if (!ft_valid_number(av[1]))
-		return (write(2, "ERROR: Please input only numbers\n", 33), -1);
-    pid = atoi(av[1]);
-    if (pid < 0)
-        return (write(1, "ERROR: U trying to kill it heheh 😑", 37), -1);
-    i = 0;
+		return (write(2, "ERROR: Please input only numbers\n", 33), 1);
+    pid = ft_atoi(av[1]);
+    if (pid == -1)
+        return (write(1, "ERROR: U trying to kill it heheh 😑", 37), 1);
     if (av[2] == NULL)
-        return (write(2, "ERROR: Pass the String!\n", 24), -1);
-    sa.sa_sigaction = &msg_received;
-	sa.sa_flags = SA_SIGINFO;
-	sigemptyset(&sa.sa_mask);
-    if (sigaction(SIGUSR1, &sa, NULL) == -1)
-		return (write(2, "error: sigaction\n", 17), -1);        
+        return (write(2, "ERROR: Pass the String!\n", 24), 1); 
+    i = 0;    
 	while (av[2][i])
 	{
-        encoding(av[2][i], pid);
+        if (encoding(av[2][i], pid) == -1)
+            return (write(2, "Error: Kill failed\n", 19), 1);
 		i++;
 	}
-    encoding('\0', pid);
+    if (encoding('\0', pid) == -1)
+            return (write(2, "Error: Kill failed\n", 19), 1);
     while(1)
         sleep(5);
     return (0);

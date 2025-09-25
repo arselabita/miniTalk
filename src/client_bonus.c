56aa0e36e5560e184d1447fdc0e6eea987368d50
Arsela Bita <arselabita>
2025-09-25 16:34:07 +0200

    i can accept the signal and aslo print it

--- src/client_bonus.c
+++ src/client_bonus.c
@@ -11,13 +11,13 @@
 /* ************************************************************************** */
 
 #define _DEFAULT_SOURCE
-#include <sys/types.h>
 #include <unistd.h>
 #include <signal.h>
-#include <stdlib.h>
-//#include "libft.h"
+#include <sys/types.h>
+#include "libft.h"
+#include "ft_printf.h"
 
-static int g_signal_received;
+volatile sig_atomic_t g_signal_received;
 
 static int	ft_valid_number(char *str)
 {
@@ -38,70 +38,73 @@ static int	ft_valid_number(char *str)
 	}
 	return (1);
 }
-static void encoding(int ascii_value, int pid)
+
+static int encoding(int ascii_value, int pid)
 {
     int j;
 
     j = 7;
     while (j >= 0)
-    {   
-        if (g_signal_received == 1)
+    {
+        if ((ascii_value >> j) & 1)
         {
-            if ((ascii_value >> j) & 1)
-            {
-                if (kill(pid, SIGUSR2) == -1)    
-                    exit(EXIT_FAILURE);
-            }
-            else
-            {
-                if (kill(pid, SIGUSR1) == -1)
-                    exit(EXIT_FAILURE);
-            }
-            g_signal_received = 0;
+            if (kill(pid, SIGUSR2) == -1)    
+                exit(EXIT_FAILURE);
+        }
+        else
+        {
+            if (kill(pid, SIGUSR1) == -1)
+                exit(EXIT_FAILURE);
         }
         usleep(100);
         j--;
     }
+    ft_printf("signal flag: %d\n", g_signal_received);
+    return (0);
 }
-void msg_received(int sig, siginfo_t *info, void *ucontext)
+
+void msg_received(int sig)
 {
-    (void)ucontext;
-    (void)info;
-    (void)sig;
-    
-    g_signal_received = 1;
+    ft_printf("entered signal handler function\n");
+    if (sig == SIGUSR1)
+    {
+        g_signal_received = 1;
+        write(1, "signal received... its hereeee!\n", 32);       
+    }
 }
-int main(int ac, char **av)
+
+static int parsing(int ac, char **av)
 {
-    struct sigaction sa;
     pid_t pid;
     int i;
 
-    g_signal_received = 1;
 	if (ac != 3)
-		return (write(2, "ERROR: Write: PID and String!\n", 30), -1);
+		return (write(2, "ERROR: Write: PID and String!\n", 30), 1);
     if (av[1] == NULL)
         return (write(2, "ERROR: Pass the PID!\n", 24), -1);
     if (!ft_valid_number(av[1]))
-		return (write(2, "ERROR: Please input only numbers\n", 33), -1);
-    pid = atoi(av[1]);
-    if (pid < 0)
-        return (write(1, "ERROR: U trying to kill it heheh 😑", 37), -1);
-    i = 0;
+		return (write(2, "ERROR: Please input only numbers\n", 33), 1);
+    pid = ft_atoi(av[1]);
+    if (pid == -1)
+        return (write(2, "ERROR: U trying to kill it heheh 😑", 37), 1);
     if (av[2] == NULL)
-        return (write(2, "ERROR: Pass the String!\n", 24), -1);
-    sa.sa_sigaction = &msg_received;
-	sa.sa_flags = SA_SIGINFO;
-	sigemptyset(&sa.sa_mask);
-    if (sigaction(SIGUSR1, &sa, NULL) == -1)
-		return (write(2, "error: sigaction\n", 17), -1);        
+        return (write(2, "ERROR: Pass the String!\n", 24), 1);
+    i = 0;    
 	while (av[2][i])
 	{
-        encoding(av[2][i], pid);
+        if (encoding(av[2][i], pid) == -1)
+            return (write(2, "Error: Kill failed\n", 19), 1);
 		i++;
 	}
-    encoding('\0', pid);
-    while(1)
-        sleep(5);
+    if (encoding('\0', pid) == -1)
+            return (write(2, "Error: Kill failed\n", 19), 1);
+    return (0);
+}
+
+int main(int ac, char **av)
+{
+    signal(SIGUSR1, msg_received);
+    if (parsing(ac, av) == 1)
+        return (write(2, "Error!\n", 7), 1);
     return (0);
 }

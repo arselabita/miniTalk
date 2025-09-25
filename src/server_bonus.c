d4e893876f45a3168d9d5a09376cee3bea2a7319
Arsela Bita <arselabita>
2025-09-25 16:35:13 +0200

    i fixed the part where it wasnt sending the signal, i needed to place it before i reset my bits and position

--- src/server_bonus.c
+++ src/server_bonus.c
@@ -11,48 +11,48 @@
 /* ************************************************************************** */
 
 #define _DEFAULT_SOURCE
-#define BUFFER_SIZE 1024
+#define ARG_MAX 1048576
 #include <sys/types.h>
 #include <unistd.h>
 #include <signal.h>
-#include <stdio.h>
 #include "ft_printf.h"
-
-static pid_t g_client_pid = 0; 
 
 static void printing(unsigned char bits)
 {
-    static char buffer[BUFFER_SIZE];
+    static char buffer[ARG_MAX];
     static int i = 0;
 
     if (bits != '\0')
     {
-        buffer[i++] = bits;
-        if (i >= BUFFER_SIZE)
+        if (i < ARG_MAX)
+            buffer[i++] = bits;
+        else
         {
-            write(1, &buffer, i);
-            i = 0;                  
+            write (1, buffer, i);
+            i = 0;
+            buffer[i++] = bits;
         }
     }
     else
     {
-        buffer[i++] = '\0';
-        write(1, &buffer, i);
+        buffer[i] = '\0';
+        write(1, buffer, i);
         write(1, "\n", 1);
         i = 0;
-        g_client_pid = 0;
     }
 }
 
 static void handler(int sig, siginfo_t *info, void *ucontext)
 {
+    (void)info;
     (void)ucontext;
     static unsigned char bits = 0;
     static int bit_position = 0;
-	
-	if (!g_client_pid)
-		g_client_pid = info->si_pid;
-    if (info->si_pid != g_client_pid)
+    static pid_t client_pid = 0;
+
+    if (!client_pid)
+        client_pid = info->si_pid;
+    if (client_pid != info->si_pid)
         return ;
     if (sig == SIGUSR1)
         bits = (bits << 1) | 0;
@@ -62,28 +62,27 @@ static void handler(int sig, siginfo_t *info, void *ucontext)
     if (bit_position == 8)
     {
         printing(bits);
+        if (bits == '\0')
+            kill(client_pid, SIGUSR1);            
         bits = 0;
         bit_position = 0;
+        client_pid = 0;
     }
-    if (info->si_pid == 0)
-        write(2, "Error: failed to get the client pid!\n", 37);
-    if (kill(g_client_pid, SIGUSR1) == -1)
-        write(2, "Error: failed to send the client pid!\n", 39); 
 }
 
 int main()
 {
     struct sigaction sa;
 
-    printf("%d\n", (int)getpid());
+    ft_printf("PID: %d\n", (int)getpid());
 	sa.sa_sigaction = &handler;
 	sa.sa_flags = SA_SIGINFO;
 	sigemptyset(&sa.sa_mask);
 	if (sigaction(SIGUSR1, &sa, NULL) == -1)
-		return (write(2, "error: sigaction\n", 17), -1);
+		return (write(2, "Error: sigaction\n", 17), -1);
 	if (sigaction(SIGUSR2, &sa, NULL) == -1)
-		return (write(2, "error: sigaction\n", 17), -1);
+		return (write(2, "Error: sigaction\n", 17), -1);
     while (1)
-        sleep(5);
+        pause();
     return (0);
 }
